d18c32b43e9fb92f3a65c71b1b70d0f4d2e5b883
François Termote <francois.termote@gmail.com>
2023-09-20 22:59:31 +0200

    bestScore 40%, paramètre 40%

--- TP/TP_promesses/main.js
+++ TP/TP_promesses/main.js
@@ -1,7 +1,14 @@
+
+
 const pokeUrl = num => `https://pokeapi.co/api/v2/pokemon/${num}` ;
 
-const pokeDex = [] ;
 
+
+const pokeDex = [] ;
+console.log("le pokeDex vient d'être initialisé, il est vide, si je l'affiche dans la console ça devrait se voire");
+console.log("d'ailleurs si j'essaie de l'afficher avant son initialisation, le script plante")
+console.log(pokeDex);
+console.log("don't trust the console.log !");
 function randPoke() {
     return Math.round(Math.random()*1009) + 1 ;
 }
@@ -26,12 +33,14 @@ function importPoke(nb) {
                 console.log("PROBLEME",error);        
             })
             .finally(() => {
-                infoPoke();
+                console.log(pokeDex.length,"itération dans le finally, i = " + i + ", pokedex correspondant : ", pokeDex);
+                if (pokeDex.length == 1) { infoPoke()};
             })
     } 
 }
 
 importPoke(10);
+
 
 console.log(pokeDex);
 
@@ -47,7 +56,7 @@ function nextPoke(nI) {
 }
 
 function infoPoke(i = 0) {
-    console.log(pokeDex.length);
+   // console.log(pokeDex.length);
     pokeInfos[0].src = pokeDex[i].urlImg;
     pokeInfos[1].textContent = `Nom : ${pokeDex[i].nom}`;
     pokeInfos[2].textContent = `Taille : ${pokeDex[i].taille}`;
